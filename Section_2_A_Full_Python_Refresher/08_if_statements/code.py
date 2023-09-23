62d84af5e0c8e1016b54a2b77a230901113af86b
Harrison Welch <harrison.r.welch@gmail.com>
2023-09-23 17:14:16 -0400

    Finish if statements

---
+++ Section_2_A_Full_Python_Refresher/08_if_statements/code.py
@@ -0,0 +1,23 @@
+# day_of_week = input("What day of the week is it today? ")
+
+# # print(day_of_week == "Monday")
+
+# if day_of_week == "Monday":
+#     # There a few spaces here for indentation
+#     # That is how python tells what code is in here.
+#     print("Have a great start to your week!")
+
+# print("This will always run")
+
+day_of_week = input("What day of the week is it today? ").lower()
+
+if day_of_week == "monday":
+    print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+    print("It's Tuesday.")
+else:
+    print("Full speed ahead!")
+
+print("This will always run")
+
+
