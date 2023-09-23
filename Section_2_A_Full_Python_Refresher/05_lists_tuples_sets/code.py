b145fb6fff39d53b297cc6fccbdea2219b92380b
Harrison Welch <harrison.r.welch@gmail.com>
2023-09-23 15:50:19 -0400

    Finish Sec 2 Lec 12 'Lists, tuples, and sets'

---
+++ Section_2_A_Full_Python_Refresher/05_lists_tuples_sets/code.py
@@ -0,0 +1,17 @@
+# l = ["Bob", "Rolf", "Anne"]
+# t = ("Bob", "Rolf", "Anne")
+# s = {"Bob", "Rolf", "Anne"}
+
+# print(l[0])
+# print(t[0])
+# print(s[0])
+
+# ----
+
+l = ["Bob", "Rolf", "Anne"]
+t = ("Bob", "Rolf", "Anne")
+s = {"Bob", "Rolf", "Anne"}
+
+s.add("Smith")
+print(s)
+
