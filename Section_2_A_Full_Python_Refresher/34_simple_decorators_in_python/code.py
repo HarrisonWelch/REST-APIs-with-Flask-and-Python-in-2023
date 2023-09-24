1c85ef9ef3c93c682de63a596b7f460b0bc23a6c
Harrison Welch <harrison.r.welch@gmail.com>
2023-09-24 17:47:57 -0400

    Simple python decorators

---
+++ Section_2_A_Full_Python_Refresher/34_simple_decorators_in_python/code.py
@@ -0,0 +1,28 @@
+user = {"username": "jose", "access_level": "admin"}
+
+
+def get_admin_password():
+    return "1234"
+
+
+# def secure_get_admin():
+#     if user["access_level"] == "admin":
+#         return "1234"
+
+
+# def secure_function(func):
+#     if user["access_level"] == "admin":
+#         return func
+
+
+def make_secure(func):
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+
+    return secure_function
+
+
+get_admin_password = make_secure(get_admin_password)
+
+print(get_admin_password())
