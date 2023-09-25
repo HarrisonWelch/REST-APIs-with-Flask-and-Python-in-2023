7969cc44a94b691fbd3d5c62e249a241209da6fc
Harrison Welch <harrison.r.welch@gmail.com>
2023-09-25 00:18:19 -0400

    Create a store in the API

--- Section_3_Your_first_REST_API/app.py
+++ Section_3_Your_first_REST_API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)  # Creates flask app that does a lot for us
 
@@ -18,3 +18,10 @@ stores = [
 @app.get("/store")  # Endpoint. http://127.0.0.1:5000/store
 def get_stores():
     return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201 # 201 is the status code. 200 is ok, 201 means accepted and going to create
